bfea11c4c2b6996cb43ee87dcc8986fc4b9a1d46
George Wright <glandonwright@gmail.com>
2019-06-06 17:16:52 -0400

    expanding user object

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ const url = require('url');
 const uuid = require('uuid');
 const bodyParser = require('body-parser');
 const util = require('util');
+const SCIMUserObject = require('./SCIMUserObject.json');
+const SCIMUserLookupTable = require('./SCIMUserLookupTable.json')
 
 const db = new sqlite3.Database('test.db'); 
 
@@ -32,26 +34,16 @@ app.use(bodyParser.json());
  */
 
  function createDBandTables() {
-    /*const createUserTable = 'CREATE TABLE Users ("id" primary key, \
-      "active" INTEGER,"userName" VARCHAR(255), \
-      "givenName" VARCHAR(255), "middleName" VARCHAR(255), \
-      "familyName" VARCHAR(255))';*/
     const createUserTable = 'CREATE TABLE Users ("id" primary key, \
     "externalId" VARCHAR(255), "userName" VARCHAR(255), \
-    "nameFormated" VARCHAR(255), "nameFamilyName" VARCHAR(255), \
+    "nameFormatted" VARCHAR(255), "nameFamilyName" VARCHAR(255), \
     "nameGivenName" VARCHAR(255), nameMiddleName VARCHAR(255), \
-    "nameHonorificPrefix" VARCHAR(255), nameHonorificSuffix VARCHAR(255), \
-    "displayName" VARCHAR(255), nickName VARCHAR(255), profileUrl VARCHAR(255), \
-    "emailsValue" VARCHAR(255), "emailsType" VARCHAR(255), "emailsPrimary" boolean, \
-    "addressesType" VARCHAR(255), "addressesStreetAddress" VARCHAR(255), \
-    "addressesLocality" VARCHAR(255), "addressesRegion" VARCHAR(255), \
-    "addressesPostalCode" VARCHAR(255), "addressesCountry" VARCHAR(255), \
-    "addressesFormatted" VARCHAR(255), "addressesPrimary" VARCHAR(255), \
-    "phoneNumbersValue" VARCHAR(255), "phoneNumbersType" VARCHAR(255), \
-    "photosValue" VARCHAR(255), "photosType" VARCHAR(255), "userType"  VARCHAR(255), \
+    "nameHonorificPrefix" VARCHAR(255), "nameHonorificSuffix" VARCHAR(255), \
+    "displayName" VARCHAR(255), "nickName" VARCHAR(255), "profileUrl" VARCHAR(255), \
+    "userType"  VARCHAR(255), \
     "title" VARCHAR(255), "preferredLanguage" VARCHAR(255),  "locale" VARCHAR(255), \
     "timezone" VARCHAR(255), "active" boolean, "password" VARCHAR(255), \
-    "x509CertificatesValue" VARCHAR(255), "metaResourceType" VARCHAR(255), \
+    "metaResourceType" VARCHAR(255), \
     "metaCreated" VARCHAR(255), "metaLastModified" VARCHAR(255), "metaVersion" VARCHAR(255), \
     "metaLocation" VARCHAR(255))';
     db.run(createUserTable, function(err) {
@@ -61,6 +53,75 @@ app.use(bodyParser.json());
       }
     });
  }
+
+ function generateUserInsertQuery(userObject) {
+  const userId = String(uuid.v1());
+  const insertQueryFields = "INSERT INTO 'Users' ('id', \
+  'externalId', 'userName', \
+  'nameFormatted', 'nameFamilyName', \
+  'nameGivenName', 'nameMiddleName', \
+  'nameHonorificPrefix', 'nameHonorificSuffix', \
+  'displayName', 'nickName', 'profileUrl', \
+  'userType' , \
+  'title', 'preferredLanguage',  'locale', \
+  'timezone', 'active', 'password', \
+  'metaResourceType', \
+  'metaCreated', 'metaLastModified', 'metaVersion', \
+  'metaLocation')";
+
+  let insertQueryValues = " VALUES ('" + userId + "','" 
+  + userObject.externalId + "','"
+  + userObject.userName + "','"
+  + userObject.name.formatted + "','"
+  + userObject.name.familyName + "','" 
+  + userObject.name.givenName + "','"
+  + userObject.name.middleName + "','"
+  + userObject.name.honorificPrefix + "','"
+  + userObject.name.honorificSuffix + "','"
+  + userObject.displayName + "','"
+  + userObject.nickName + "','"
+  + userObject.profileUrl + "','"
+  + userObject.userType + "','"
+  + userObject.title + "','"
+  + userObject.preferredLanguage + "','"
+  + userObject.locale + "','"
+  + userObject.timezone + "','"
+  + userObject.active + "','"
+  + userObject.password + "','"
+  + userObject.meta.resourceType + "','"
+  + userObject.meta.created + "','"
+  + userObject.meta.lastModified + "','"
+  + "W\/\"a330bc54f0671c9\"" + "','"
+  + userObject.meta.location + "')";
+
+  return insertQueryFields + insertQueryValues;
+}
+
+function generateUpdateUsersQuery(userObject, userId) {
+  return "UPDATE 'Users' SET externalId = '" + String(userObject.externalId)
+  + "', userName = '" + String(userObject.userName) + "', nameFormatted ='"
+  + String(userObject.name.formatted) + "', nameFamilyName= '" + String(userObject.name.familyName)
+  + "', nameGivenName ='" + String(userObject.name.givenName)
+  + "', nameMiddleName ='" + String(userObject.name.middleName)
+  + "', nameHonorificPrefix ='" + String(userObject.name.honorificPrefix)
+  + "', nameHonorificSuffix ='" + String(userObject.name.honorificSuffix)
+  + "', displayName ='" + String(userObject.displayName)
+  + "', nickName ='" + String(userObject.nickName)
+  + "', profileUrl ='" + String(userObject.profileUrl)
+  + "', userType ='" + String(userObject.userType)
+  + "', title ='" + String(userObject.title)
+  + "', preferredLanguage ='" + String(userObject.preferredLanguage)
+  + "', locale ='" + String(userObject.locale)
+  + "', timezone ='" + String(userObject.timezone)
+  + "', active ='" + String(userObject.active)
+  + "', password ='" + String(userObject.password)
+  + "', metaResourceType ='" + String(userObject.meta.resourceType)
+  + "', metaCreated ='" + String(userObject.meta.created)
+  + "', metaLastModified ='" + String(new Date())
+  + "', metaVersion ='" + String("W\/\"a330bc54f0671c9\"")
+  + "', metaLocation ='" + String(userObject.meta.location)
+  + "' WHERE id = '" + userId + "'";
+}
 
 /** 
  *   Constructor for creating SCIM Resource 
@@ -80,14 +141,7 @@ function GetSCIMList(rows, startIndex, count, req_url) {
   let location = ""
   for (let i = (startIndex-1); i < count; i++) {
     location =  req_url + "/" + rows[i]["id"];
-    let userResource = GetSCIMUserResource(
-      rows[i]["id"],
-      rows[i]["active"],
-      rows[i]["userName"],
-      rows[i]["givenName"],
-      rows[i]["middleName"],
-      rows[i]["familyName"],
-      location);
+    let userResource = GetSCIMUserResource(rows[i], location);
     resources.push(userResource);
     location = "";
   }
@@ -100,38 +154,36 @@ function GetSCIMList(rows, startIndex, count, req_url) {
   return scim_resource;
 }
 
-/**
- *  Returns JSON dictionary of SCIM response
- */
-function GetSCIMUserResource(userId, active, userName,
-  givenName, middleName, familyName, req_url) {
+function GetSCIMUserResource(row, req_url) {
+  
+  let scim_user = SCIMUserObject;
 
-  let scim_user = {
-    "schemas": [ "urn:ietf:params:scim:schemas:core:2.0:User" ],
-    "id": null,
-    "userName": null,
-    "name": {
-      "givenName": null,
-      "middleName": null,
-      "familyName": null,
-    },
-    "active": false,
-    "meta": {
-      "resourceType": "User",
-      "location": null,
-    }
-  };
-
-  scim_user["meta"]["location"] = req_url;
-  scim_user["id"] = userId;
-  scim_user["active"] = active;
-  scim_user["userName"] = userName;
-  scim_user["name"]["givenName"] = givenName;
-  scim_user["name"]["middleName"] = middleName;
-  scim_user["name"]["familyName"] = familyName;
-
+  scim_user.id = row.id;
+  scim_user.externalId = row.externalId; 
+  scim_user.userName = row.userName;
+  scim_user.name.formatted = row.nameFormatted; 
+  scim_user.name.familyName = row.nameFamilyName;
+  scim_user.name.givenName = row.nameGivenName;
+  scim_user.name.middleName = row.nameMiddleName
+  scim_user.name.honorificPrefix = row.nameHonorificPrefix
+  scim_user.name.honorificSuffix = row.nameHonorificSuffix
+  scim_user.displayName = row.displayName
+  scim_user.nickName = row.nickName
+  scim_user.profileUrl = row.profileUrl
+  scim_user.userType = row.userType
+  scim_user.title = row.title
+  scim_user.preferredLanguage = row.preferredLanguage
+  scim_user.locale = row.locale
+  scim_user.timezone = row.timezone
+  scim_user.active = row.active
+  scim_user.password = row.password
+  scim_user.meta.resourceType = row.metaResourceType
+  scim_user.meta.created = row.metaCreated
+  scim_user.meta.lastModified = row.metaLastModified
+  scim_user.meta.version = row.metaVersion
+  scim_user.meta.location = req_url;
+   
   return scim_user;
-
 }
 
 /**
@@ -156,54 +208,40 @@ function SCIMError(errorMessage, statusCode) {
 app.post('/scim/v2/Users',  function (req, res) {   
   const url_parts = url.parse(req.url, true);
   const req_url =  url_parts.pathname;
-
-  const active = req.body.active;
-  const userName = req.body.userName;
-  const givenName = req.body.name.givenName;
-  const middleName = req.body.name.middleName;
-  const familyName = req.body.name.familyName;
-  
-
-  const usernameQuery = "SELECT * FROM Users WHERE userName='" + userName + "'";
+  const usernameQuery = "SELECT * FROM Users WHERE userName='" + req.body.userName + "'";
   db.get(usernameQuery, function(err, rows) {
-    if (err == null) {
-      if (rows === undefined) {
-        const userId = String(uuid.v1());
-        const runQuery = "INSERT INTO 'Users' (id, active, userName, givenName,\
-                     middleName, familyName) VALUES ('" + userId + "','" 
-                     + active + "','" + userName + "','" + givenName + "','" + middleName + "','"
-                     + familyName + "')";
-        db.run(runQuery, function(err) {
-          if (err !== null) {
+    if (err) {
+      const scim_error = SCIMError( String(err), "400");
+      res.writeHead(400, {'Content-Type': 'text/plain'});
+      res.end(JSON.stringify(scim_error));  
+    }
+
+    else {
+      if (rows) {
+        const scim_error = SCIMError( "Conflict - Resource Already Exists", "409");
+        res.writeHead(409, {'Content-Type': 'text/plain'});
+        res.end(JSON.stringify(scim_error));
+      }
+      //insert new user
+      else {
+        const query = generateUserInsertQuery(req.body);
+        console.log(query);
+        db.run(query, function(err) {
+          if (err) {
             const scim_error = SCIMError( String(err), "400");
             res.writeHead(400, {'Content-Type': 'text/plain'});
             res.end(JSON.stringify(scim_error));
-          } 
-          
-          else {
-            const scimUserResource = GetSCIMUserResource(userId, active, userName,
-              givenName, middleName, familyName, req_url); 
-          
-            res.writeHead(201, {'Content-Type': 'text/json'});
-            res.end(JSON.stringify(scimUserResource));
           }
-        });              
-      } 
-      
-      else {
-          const scim_error = SCIMError( "Conflict - Resource Already Exists", "409");
-          res.writeHead(409, {'Content-Type': 'text/plain'});
-          res.end(JSON.stringify(scim_error));
-        }
-    } 
-    
-    else {
-        const scim_error = SCIMError( String(err), "400");
-        res.writeHead(400, {'Content-Type': 'text/plain'});
-        res.end(JSON.stringify(scim_error));
+
+          else {
+            res.writeHead(201, {'Content-Type': 'text/json'});
+            res.end(JSON.stringify('created'));
+          }
+        });
       }
-  
-  }); 
+    }
+  });
+  generateUserInsertQuery(req.body);
 });
 
 /**
@@ -248,7 +286,9 @@ app.get("/scim/v2/Users", function (req, res) {
           res.writeHead(200, {'Content-Type': 'application/json'})
           res.end(JSON.stringify(scimResource))
         }
-    } else {
+    } 
+    
+    else {
         const scim_error = SCIMError( String(err), "400");
         res.writeHead(400, {'Content-Type': 'text/plain'});
         res.end(JSON.stringify(scim_error));
@@ -285,8 +325,7 @@ app.get("/scim/v2/Users/:userId", function (req, res){
       }
 
       else {
-        const scimUserResource = GetSCIMUserResource(userId, rows.active, rows.userName,
-          rows.givenName, rows.middleName, rows.familyName, req_url);         
+        const scimUserResource = GetSCIMUserResource(rows, req_url);         
           res.writeHead(200, {'Content-Type': 'application/json'})
           res.end(JSON.stringify(scimUserResource));
       }
@@ -299,10 +338,31 @@ app.get("/scim/v2/Users/:userId", function (req, res){
 app.put("/scim/v2/Users/:userId", function (req, res) {
 	const userId = req.params.userId;
 	const url_parts = url.parse(req.url, true);
-	const req_url = url_parts.pathname;
+  const req_url = url_parts.pathname;
+  console.log(req.body.userId);
+  const queryById = "SELECT * FROM Users WHERE id='" + userId + "'";
+  db.get(queryById, function(err, rows) {
+    if (err) {
+      const scim_error = SCIMError( String(err), "400");
+      res.writeHead(400, {'Content-Type': 'text/plain'});
+      res.end(JSON.stringify(scim_error));
+    }
 
+    else {
+      if (rows) {
+        const updateQuery = generateUpdateUsersQuery(req.body, userId);
+        console.log(updateQuery);
+        //write logic to execute query and should be good to go
+      }
 
-  const active = req.body.active;
+      else {
+        const scim_error = SCIMError( "User Does Not Exist", "404");
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end(JSON.stringify(scim_error));
+      }
+    }
+  });
+  /*const active = req.body.active;
   const userName = req.body.userName;
   const givenName = req.body.name.givenName;
   const middleName = req.body.name.middleName;
@@ -345,8 +405,7 @@ app.put("/scim/v2/Users/:userId", function (req, res) {
         res.writeHead(400, {'Content-Type': 'text/plain'});
         res.end(JSON.stringify(scim_error));
       }
-  });
-
+  });*/
 });
 
 app.delete("/scim/v2/Users/:userID", function(req, res) {
@@ -367,8 +426,6 @@ app.delete("/scim/v2/Users/:userID", function(req, res) {
 app.post('/scim/v2/Groups/', function(req, res) {
   const url_parts = url.parse(req.url, true);
   const req_url =  url_parts.pathname;
-
-  
 });
 
 /**
